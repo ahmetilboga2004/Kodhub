faa3a8814ce6d525c8a244d2f4e01167156ca2e4
ahmetilboga2004 <ilbogaahmet4747@gmail.com>
2024-10-20 21:20:14 +0300

    Changed notification component icon

--- frontend/src/components/NotificationItem.vue
+++ frontend/src/components/NotificationItem.vue
@@ -2,11 +2,7 @@
     <div class="dropdown dropdown-end z-50">
         <label tabindex="0" class="btn btn-ghost btn-circle">
             <div class="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                </svg>
+                <svgIconVue type="mdi" :path="mdiBellOutline"></svgIconVue>
                 <span class="badge badge-sm indicator-item" v-if="notifications.length">{{ notifications.length
                     }}</span>
             </div>
@@ -37,7 +33,7 @@ import { ref, onMounted, onUnmounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/auth';
 import svgIconVue from '@jamescoyle/vue-icon';
-import { mdiEyeCheckOutline } from '@mdi/js';
+import { mdiEyeCheckOutline, mdiBellOutline } from '@mdi/js';
 
 
 const authStore = useAuthStore()
